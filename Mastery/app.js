219654033ccf0fbbbba9b930a6d9679ad87d419b
Muhammad Arslan <imarslan7061@gmail.com>
2026-06-14 13:14:39 +0500

    OS Module

--- Mastery/app.js
+++ Mastery/app.js
@@ -1,3 +1,15 @@
+import os from "node:os"
+console.log("CPU's", os.cpus())
+console.log("Total Memorey:", (os.totalmem() / (1024 * 1024 * 1024)).toFixed(2))
+console.log("Free Memorey:", os.freemem() / (1024 * 1024 * 1024))
+console.log("Uptime:", os.uptime() / (60 * 60))
+console.log("HostName:", os.hostname())
+console.log("UserInfo:", os.userInfo())
+console.log("Architecture:", os.machine())
+
+
+
+
 // // Common JS
 // // const getGreetings = require("./greeter");
 
