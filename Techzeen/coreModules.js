cb1fdb85b325c521a271a1b5f9640c2ead134d42
Muhammad Arslan <imarslan7061@gmail.com>
2026-06-18 11:49:46 +0500

    Core Modules

---
+++ Techzeen/coreModules.js
@@ -0,0 +1,5 @@
+// Core Modules
+// 1- Global Core Module (Jin ko use krne ke liye imprkrt krwane ki zarora nhi prti like console)
+// 2- Local Core Module (Import krwana prt ha use krne keliyia.)
+const myFile = require("fs").writeFileSync
+myFile("./Techzeen/coreModule.txt", "Core Modules in JS")
