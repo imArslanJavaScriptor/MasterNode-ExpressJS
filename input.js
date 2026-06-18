ce5915fb097ba91ae0f7614f67352a0aef7dbfd1
Muhammad Arslan <imarslan7061@gmail.com>
2026-06-18 19:42:57 +0500

    fs module with program

---
+++ input.js
@@ -0,0 +1,17 @@
+import fs from "fs"
+
+// console.log("Process", process.argv)
+// console.log(process.argv[2])
+
+
+const action = process.argv[2]
+const fileName = process.argv[3]
+const fileData = process.argv[4]
+
+if(action == "create") {
+    fs.writeFileSync(fileName, fileData)
+} else if(action == "delete") {
+    fs.unlinkSync(fileName)
+}else {
+    console.log("Invalid Action")
+}
