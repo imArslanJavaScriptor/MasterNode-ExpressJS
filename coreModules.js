aab521a3a160bf34459c0e733ee05df95f893e6e
Muhammad Arslan <imarslan7061@gmail.com>
2026-06-18 18:12:00 +0500

    nodemon discussion

--- coreModules.js
+++ coreModules.js
@@ -2,4 +2,4 @@
 // 1- Global Core Module (Jin ko use krne ke liye imprkrt krwane ki zarora nhi prti like console)
 // 2- Local Core Module (Import krwana prt ha use krne keliyia.)
 const myFile = require("fs").writeFileSync
-myFile("./Techzeen/coreModule.txt", "Core Modules in JS")
+myFile("./coreModule.txt", "Core Modules in JS")
