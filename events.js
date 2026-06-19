a77325783e9dc019cd21e85a4375aeddacccdf46
Muhammad Arslan <imarslan7061@gmail.com>
2026-06-19 16:28:36 +0500

    express js intro

--- events.js
+++ events.js
@@ -1,15 +1,18 @@
-const fs = require("fs")
-const myRead = fs.createReadStream("./coreModule.txt")
+// Builtin event listeners
+// const fs = require("fs")
+// const myRead = fs.createReadStream("./coreModule.txt")
 
-myRead.on("open", () => {
-    console.log("coreModule.txt is opened")
-})
+// myRead.on("open", () => {
+//     console.log("coreModule.txt is opened")
+// })
 
+
+// Customize event listeners
 const event = require("events")
 const eventEmmiter = new event.EventEmitter()
 
-eventEmmiter.on("play", (type) => {
+eventEmmiter.on("sport", (type) => {
     console.log(`I'm Playing ${type}`)
 })
 
-eventEmmiter.emit("play", "Cricket")
+eventEmmiter.emit("sport", "Cricket")
