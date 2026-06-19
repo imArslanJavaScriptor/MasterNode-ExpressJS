537f401c52cb3abba139e375cdb6856c025e035c
Muhammad Arslan <imarslan7061@gmail.com>
2026-06-19 06:28:05 +0500

    Promises

--- asynchronous.js
+++ asynchronous.js
@@ -12,10 +12,40 @@
 
 
 // Exmaple of Asnychrnous Disadvantageous
+// let firstName = "HM"
+// let lastName = ""
+// setTimeout(() => {
+//     lastName  = "Arslan"
+// }, 2000);
+
+// console.log(`${firstName} ${lastName}`)
+
+
+// Handle with Promises
 let firstName = "HM"
 let lastName = ""
-setTimeout(() => {
-    lastName  = "Arslan"
-}, 2000);
+const setLastName = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // lastName = "Arslan"
+        resolve(lastName = "Arslan")  
+    }, 2000);
+})
 
-console.log(`${firstName} ${lastName}`)
+// setLastName.then((val) => {
+//     console.log(`${firstName} ${lastName}`)
+//     console.log(val)
+// })
+
+
+// async / await
+// setLastName.then((val) => {
+//     console.log(`${firstName} ${lastName}`)
+//     console.log(val)
+// })
+
+const displayFullName = async () => {
+    lastName = await setLastName;
+    console.log(`${firstName} ${lastName}`)
+}
+
+displayFullName()
