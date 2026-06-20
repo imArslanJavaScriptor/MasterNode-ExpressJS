5b6b4bedaefe05d88a56ef3f440855b245032db9
Muhammad Arslan <imarslan7061@gmail.com>
2026-06-21 03:05:47 +0500

    Redering Static Files

---
+++ express/renderStaticPages.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const staticPath = path.join(__dirname, "public")
+
+
+app.use(express.static(staticPath))
+
+app.listen(3000)
